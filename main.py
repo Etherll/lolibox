f779614785a79917bc5fb784d785cc307b4284c9
Etherll <61019402+Etherll@users.noreply.github.com>
2022-02-05 00:15:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import io
 import requests
 import keyboard
-from PyQt5.QtCore import Qt,QPoint,QRect
+from PyQt5.QtCore import Qt,QPoint,QRect,QThread,pyqtSignal
 from PIL import ImageQt,ImageGrab ,ImageEnhance
 
 class MainWindow(QMainWindow):
@@ -52,6 +52,7 @@ class MainWindow(QMainWindow):
     def mouseReleaseEvent(self, event):
         self.begin = event.pos()
         self.end = event.pos()
+        
         area = (self.first.x(),self.second.y() ,self.begin.x(),self.end.y())
         if self.first.x() > self.begin.x():
             area = (self.begin.x(),self.second.y(),self.first.x(),self.end.y())
@@ -60,32 +61,40 @@ class MainWindow(QMainWindow):
         if self.first.x() > self.begin.x() and self.second.y() > self.begin.y():
             area = (self.begin.x(),self.begin.y(),self.first.x(),self.second.y())
         self.screenshot = self.screenshot.crop(area)
-
+        output = io.BytesIO()
+        self.screenshot.convert("RGB").save(output, "jpeg")
+        files= {'photo': ('61f597c8bcef3a0032d37c1c.jpg',output.getvalue(),'image/jpeg')}
+        self.worker = GetData()
+        self.worker.files = files
+        self.worker.start()
+        self.worker.clean_text.connect(self.copy_text)
+    def copy_text(self, text):
         cb = QApplication.clipboard()
-        
-        def senddata():
-            output = io.BytesIO()
-            self.screenshot.convert("RGB").save(output, "jpeg")
-            files= {'photo': ('61f597c8bcef3a0032d37c1c.jpg',output.getvalue(),'image/jpeg')}
-            req = requests.post('https://blackboxapp.co/getsingleimage',files = files).json()
-            
-            if req !='Error':
-                cb.clear()
-                cb.setText(req['text'])
-                return self.close()
-            elif req == 'Error':
-                senddata()
-            else:
-                print('error',req)
+        cb.clear()
+        cb.setText(text)
+        self.close()
 
-        senddata()
-        
+class GetData(QThread):
+    files = {}
+    clean_text = pyqtSignal(str)
+    def senddata(self):
+        req = requests.post('https://blackboxapp.co/getsingleimage',files=self.files).json()
+        if req !='Error':
+            return self.clean_text.emit(req['text'].replace('%3D',''))
+        elif req == 'Error':
+            self.senddata()
+        else:
+            print('error',req)
+    def run(self):
+        self.senddata()
+
 def run():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     app.exec_()
-    
 hot_key = json.loads(open("config.json","r").read())
+
 keyboard.add_hotkey(hot_key['key'], run) 
-keyboard.wait('esc') 
+  
+keyboard.wait()
